bba703833b7a296d98305596a0138454bc8ba234
vitormarcal <vitormarcal8@gmail.com>
2017-10-12 13:05:32 -0300

    primeiro commit

---
+++ src/main/java/com/vitormarcal/asteya/AsteyaApplication.java
@@ -0,0 +1,12 @@
+package com.vitormarcal.asteya;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsteyaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsteyaApplication.class, args);
+	}
+}
