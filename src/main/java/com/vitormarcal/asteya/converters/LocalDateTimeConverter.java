7e4004d299782e0ac1389c8da4415ad7c23853c0
vitormarcal <vitormarcal8@gmail.com>
2017-10-14 10:13:30 -0300

    adicionado converter local date para date e vice-versa

--- src/main/java/com/vitormarcal/asteya/converters/LocalDateTimeConverter.java
+++ src/main/java/com/vitormarcal/asteya/converters/LocalDateTimeConverter.java
@@ -6,12 +6,12 @@ import java.sql.Timestamp;
 import java.time.LocalDateTime;
 import java.util.Optional;
 
-@SuppressWarnings("unused")
-@Converter(autoApply = true)
 /**
  * Converte LocalDateTime da Entity para um TimeStamp e vice-versa.
  * Evita problemas de compatibilidade do java 8 com o ORM.
  */
+@SuppressWarnings("unused")
+@Converter(autoApply = true)
 public class LocalDateTimeConverter implements AttributeConverter<LocalDateTime, Timestamp> {
 
     @Override
