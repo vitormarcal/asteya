0bf5aa6a3a1f08ffeb1ad63d59e74a893c016ad5
vitormarcal <vitormarcal8@gmail.com>
2017-10-14 09:05:24 -0300

    adicionado exception handler

---
+++ src/main/java/com/vitormarcal/asteya/exceptionhandlers/AsteyaExceptionHandler.java
@@ -0,0 +1,67 @@
+package com.vitormarcal.asteya.exceptionhandlers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@ControllerAdvice
+public class AsteyaExceptionHandler extends ResponseEntityExceptionHandler {
+
+    private final MessageSource messageSource;
+
+
+    @Autowired
+    public AsteyaExceptionHandler(MessageSource messageSource) {
+        this.messageSource = messageSource;
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        List<Erro> erros = criarListaDeErros(ex.getBindingResult());
+        return super.handleExceptionInternal(ex, erros, headers, HttpStatus.BAD_REQUEST, request);
+    }
+
+    private List<Erro> criarListaDeErros(BindingResult bindingResult) {
+        List<Erro> erros = new ArrayList<>();
+
+        bindingResult.getFieldErrors().forEach(fieldError -> {
+            String mensagemUsuario = messageSource.getMessage(fieldError, LocaleContextHolder.getLocale());
+            String mensagemDesenvolvedor = fieldError.toString();
+            erros.add(new Erro(mensagemUsuario, mensagemDesenvolvedor));
+        });
+        return erros;
+    }
+
+    public static class Erro {
+        private String mensagemUsuario;
+        private String mensagemDesenvolvedor;
+
+        public Erro(String mensagemUsuario, String mensagemDesenvolvedor) {
+            super();
+            this.mensagemUsuario = mensagemUsuario;
+            this.mensagemDesenvolvedor = mensagemDesenvolvedor;
+        }
+
+        public String getMensagemDesenvolvedor() {
+            return mensagemDesenvolvedor;
+        }
+
+        public String getMensagemUsuario() {
+            return mensagemUsuario;
+        }
+
+    }
+}
