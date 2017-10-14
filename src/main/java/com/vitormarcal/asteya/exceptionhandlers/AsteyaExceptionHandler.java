628ab29b9fd9c65a59407f878554c3ef1f1e786c
vitormarcal <vitormarcal8@gmail.com>
2017-10-14 09:54:15 -0300

    refatoração de lint

--- src/main/java/com/vitormarcal/asteya/exceptionhandlers/AsteyaExceptionHandler.java
+++ src/main/java/com/vitormarcal/asteya/exceptionhandlers/AsteyaExceptionHandler.java
@@ -3,7 +3,6 @@ package com.vitormarcal.asteya.exceptionhandlers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
-import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,12 +10,11 @@ import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 @ControllerAdvice
@@ -36,7 +34,7 @@ public class AsteyaExceptionHandler extends ResponseEntityExceptionHandler {
 
         String mensagemUsuario = messageSource.getMessage("mensagem.invalida", null, LocaleContextHolder.getLocale());
         String mensagemDesenvolvedor = ex.getCause() != null ? ex.getCause().toString() : ex.toString();
-        List<Erro>erros = Arrays.asList(new Erro(mensagemUsuario, mensagemDesenvolvedor));
+        List<Erro>erros = Collections.singletonList(new Erro(mensagemUsuario, mensagemDesenvolvedor));
         return handleExceptionInternal(ex, erros, headers, HttpStatus.BAD_REQUEST, request);
     }
 
@@ -61,16 +59,18 @@ public class AsteyaExceptionHandler extends ResponseEntityExceptionHandler {
         private String mensagemUsuario;
         private String mensagemDesenvolvedor;
 
-        public Erro(String mensagemUsuario, String mensagemDesenvolvedor) {
+        private Erro(String mensagemUsuario, String mensagemDesenvolvedor) {
             super();
             this.mensagemUsuario = mensagemUsuario;
             this.mensagemDesenvolvedor = mensagemDesenvolvedor;
         }
 
+        @SuppressWarnings("unused")
         public String getMensagemDesenvolvedor() {
             return mensagemDesenvolvedor;
         }
 
+        @SuppressWarnings("unused")
         public String getMensagemUsuario() {
             return mensagemUsuario;
         }
