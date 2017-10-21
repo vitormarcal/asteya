5298a83612fa7e0e92804842e00409aea824c28c
vitormarcal <vitormarcal8@gmail.com>
2017-10-21 16:02:47 -0200

    refactor: divida resource implements BaseResource

--- src/main/java/com/vitormarcal/asteya/features/dividas/DividaResource.java
+++ src/main/java/com/vitormarcal/asteya/features/dividas/DividaResource.java
@@ -1,6 +1,6 @@
 package com.vitormarcal.asteya.features.dividas;
 
-import com.vitormarcal.asteya.events.RecursoCriadoEvent;
+import com.vitormarcal.asteya.features.BaseResource;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,7 +16,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/dividas")
 @Api(value = "dividas", tags = {"Manter dividas"})
-public class DividaResource {
+public class DividaResource implements BaseResource<Divida>{
 
     private final DividaService dividaService;
     private final ApplicationEventPublisher publisher;
@@ -27,50 +27,38 @@ public class DividaResource {
         this.publisher = publisher;
     }
 
-    @GetMapping
+    @Override
     @ApiOperation(value = "Lista todas as dívidas cadastradas", response = Divida.class, responseContainer = "List")
     public List<Divida> listar() {
         return dividaService.listar();
     }
 
-    @GetMapping("/{id}")
+    @Override
     @ApiOperation(value = "Busca uma dívida cadastrada", response = Divida.class)
     public ResponseEntity<Divida> buscar(@PathVariable("id") Long idDivida) {
         Divida divida =  dividaService.buscar(idDivida);
         return divida != null ? ResponseEntity.ok(divida) : ResponseEntity.notFound().build();
     }
 
-    @PostMapping
-    @ResponseStatus(HttpStatus.CREATED)
+    @Override
     @ApiOperation(value = "Guarda uma dívida", response = Divida.class)
     public ResponseEntity<Divida> salvar(@RequestBody @Valid Divida divida, HttpServletResponse response) {
         Divida dividaBanco = dividaService.salvar(divida);
-        publicaLocalizacaoRecurso(dividaBanco.getId(), response);
+        publicaLocalizacaoRecurso(dividaBanco.getId(), response, publisher);
         return ResponseEntity.status(HttpStatus.CREATED).body(dividaBanco);
     }
 
-    @PutMapping("{id}")
+    @Override
     @ApiOperation(value = "Atualiza uma dívida", response = Divida.class)
     public ResponseEntity<Divida> atualizar(@PathVariable("id") Long idDivida, @RequestBody @Valid Divida divida) {
         Divida dividaAtualizada = dividaService.atualizar(idDivida, divida);
         return ResponseEntity.ok(dividaAtualizada);
     }
 
-    @DeleteMapping("{id}")
-    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @Override
     @ApiOperation(value = "Remove uma dívida")
     public void remover(@PathVariable("id") Long idDivida) {
         dividaService.remover(idDivida);
-    }
-
-    /**
-     * Aciona evento que escreve no header do response o Location (localização) do recurso criado.
-     * @param id codigo identificador do recurso no banco de dados
-     * @param response response da requisição que será adicionado o Location
-     * @see RecursoCriadoEvent
-     */
-    private void publicaLocalizacaoRecurso(Long id, HttpServletResponse response) {
-        publisher.publishEvent(new RecursoCriadoEvent(this, response, id));
     }
 
 }
