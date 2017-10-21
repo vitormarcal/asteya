351ec5f89f52a923a9302f4b020a978ee7e36647
vitormarcal <vitormarcal8@gmail.com>
2017-10-21 14:39:58 -0200

    adicionado base entity e mudado rota do scanner do swagger

---
+++ src/main/java/com/vitormarcal/asteya/features/BaseEntity.java
@@ -0,0 +1,20 @@
+package com.vitormarcal.asteya.features;
+
+import io.swagger.annotations.ApiModelProperty;
+import lombok.*;
+
+import javax.persistence.*;
+
+@MappedSuperclass
+@NoArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode(of = "id") @ToString
+public class BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    @ApiModelProperty(notes = "O identificador gerado pelo database")
+    protected Long id;
+}
